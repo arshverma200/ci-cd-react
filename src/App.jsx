068f5e9805b176ee2fa53726e6a8c3a13449f69c
arshverma200 <vermaarsh200@gmail.com>
2025-05-01 20:33:42 +0530

    First Commit CI/Cd

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div> 
+    <p>My React App</p>
+   <p>This is a simple react app Implement with CI/CD pipeline</p>
+   
+   </div>
+  )
+}
+
+export default App
