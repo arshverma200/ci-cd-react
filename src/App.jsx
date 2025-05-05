aeee803311f87dfd0331cd8b9db3a9b65b007936
Arsh Verma <123056068+arshverma200@users.noreply.github.com>
2025-05-05 20:37:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
    <div> 
     <p>My React App</p>
-   <p>This is a simple react app Implement with CI/CD pipeline</p>
+   <p>This is a simple react app Implement with CI/CD pipelines ss</p>
    
    </div>
   )
